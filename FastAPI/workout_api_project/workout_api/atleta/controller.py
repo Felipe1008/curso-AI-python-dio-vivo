bb6f9959117eb1c3c5af56821e9ec74633f72a1d
felipe <felipediaspigiani@gmail.com>
2025-03-24 16:50:22 -0300

    fix: correct CRUD implementation

--- FastAPI/workout_api_project/workout_api/atleta/controller.py
+++ FastAPI/workout_api_project/workout_api/atleta/controller.py
@@ -26,25 +26,32 @@ async def post(db_session: DatabaseDependency, atleta_in: AtletaIn = Body(...)):
     if not categoria:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail='A categoria {categoria_nome} não foi encontrada.'
+            detail=f'A categoria {categoria_nome} não foi encontrada.'
         )
     
     centro_treinamento = (await db_session.execute(select(CentroTreinamentoModel).filter_by(nome=centro_treinamento_nome))).scalars().first()
     if not centro_treinamento:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail='O CT {centro_treinamento_nome} não foi encontrado.'
+            detail=f'O CT {centro_treinamento_nome} não foi encontrado.'
         )
     
     try:
-        atleta_out = AtletaOut(id=uuid4(), created_at=datetime.now(timezone.utc), **atleta_in.model_dump())
+        atleta_out = AtletaOut(id=uuid4(), created_at=datetime.now(timezone.utc).replace(tzinfo=None), **atleta_in.model_dump())
         atleta_model = AtletaModel(**atleta_out.model_dump(exclude={'categoria', 'centro_treinamento'}))
 
         atleta_model.categoria_id = categoria.pk_id
         atleta_model.centro_treinamento_id = centro_treinamento.pk_id
-
-        db_session.add(atleta_model)
-        await db_session.commit()
+        try:
+            db_session.add(atleta_model)
+            await db_session.commit()
+        except Exception as e:
+            print(f"Erro ao salvar no banco: {e}")
+            await db_session.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"Erro ao salvar no banco: {str(e)}"
+            )
     except Exception:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -62,7 +69,7 @@ async def post(db_session: DatabaseDependency, atleta_in: AtletaIn = Body(...)):
 async def query(db_session: DatabaseDependency) -> list[AtletaOut]:
     atletas: list[AtletaOut] = (await db_session.execute(select(AtletaModel))).scalars().all()
 
-    return [AtletaOut.model_validate() for atleta in atletas]
+    return [AtletaOut.model_validate(atleta) for atleta in atletas]
 
 @router.get(
     path='/{id}',
@@ -71,7 +78,7 @@ async def query(db_session: DatabaseDependency) -> list[AtletaOut]:
     response_model=AtletaOut
 )
 async def get_by_id(id: UUID4, db_session: DatabaseDependency):
-    atleta: AtletaOut = (await db_session.execute(select(AtletaModel)).filter_by(id=id)).scalars().first()
+    atleta: AtletaOut = (await db_session.execute(select(AtletaModel).filter_by(id=id))).scalars().first()
 
     if not atleta:
         raise HTTPException(
@@ -88,7 +95,7 @@ async def get_by_id(id: UUID4, db_session: DatabaseDependency):
     response_model=AtletaOut
 )
 async def get(id: UUID4, db_session: DatabaseDependency, atleta_up: AtletaUpdate = Body(...)) -> AtletaOut:
-    atleta: AtletaOut = (await db_session.execute(select(AtletaModel)).filter_by(id=id)).scalars().first()
+    atleta: AtletaOut = (await db_session.execute(select(AtletaModel).filter_by(id=id))).scalars().first()
 
     if not atleta:
         raise HTTPException(
@@ -111,7 +118,7 @@ async def get(id: UUID4, db_session: DatabaseDependency, atleta_up: AtletaUpdate
     status_code=status.HTTP_204_NO_CONTENT,
 )
 async def delete(id: UUID4, db_session: DatabaseDependency) -> None:
-    atleta: AtletaOut = (await db_session.execute(select(AtletaModel)).filter_by(id=id)).scalars().first()
+    atleta: AtletaOut = (await db_session.execute(select(AtletaModel).filter_by(id=id))).scalars().first()
 
     if not atleta:
         raise HTTPException(
